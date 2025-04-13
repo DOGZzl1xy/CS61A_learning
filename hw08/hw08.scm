4baa0a361b7c6feabd60c9475a7c494a273b2931
TangyuanNewITX <zhk3801258@outlook.com>
2025-04-12 18:57:29 -0700

    finish hw08

--- hw08/hw08.scm
+++ hw08/hw08.scm
@@ -1,7 +1,35 @@
-(define (ascending? s) 'YOUR-CODE-HERE)
+(define (ascending? s) 
 
-(define (my-filter pred s) 'YOUR-CODE-HERE)
+(cond ((null? s) #t)
+((null? (cdr s)) #t)
+(else (and (<= (car s) (car (cdr s))) (ascending? (cdr s))))
+)
+)
 
-(define (interleave lst1 lst2) 'YOUR-CODE-HERE)
+(define (my-filter pred s) 
 
-(define (no-repeats s) 'YOUR-CODE-HERE)
+(cond ((null? s) '())
+((pred (car s)) (cons (car s) (my-filter pred (cdr s))))
+(else (my-filter pred (cdr s)))
+
+)
+
+)
+
+(define (interleave lst1 lst2) 
+(cond ((null? lst1) lst2)
+((null? lst2) lst1)
+(else (cons (car lst1) (interleave lst2 (cdr lst1))))
+    
+
+)
+
+)
+
+(define (no-repeats s) 
+(cond ((null? s) '())
+((null? (my-filter (lambda (x) (= (car s) x) ) (cdr s))) (cons (car s) (no-repeats (cdr s))))
+(else (no-repeats (cdr s)))
+
+)
+)
